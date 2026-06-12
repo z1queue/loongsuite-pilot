5db05ad4884c499c5f439ae7f1804b77c16ec26b
Monsooooon <66199082+Monsooooon@users.noreply.github.com>
2026-06-12 13:27:31 +0800

    add .d.ts (#2)

---
+++ src/internal/alarm-sender.internal.d.ts
@@ -0,0 +1,2 @@
+export declare function sendAlarm(topic: string, data: Record<string, unknown>): void;
+export declare function sendStatus(topic: string, data: Record<string, unknown>): void;
---
+++ src/internal/statistic.internal.d.ts
@@ -0,0 +1,1 @@
+export declare function sendRunningStatus(data: Record<string, unknown>): void;
